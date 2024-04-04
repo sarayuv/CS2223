b102b5378b32d04611412a827cc6aab7931cbe02
Sarayu Vijayanagaram <147265985+sarayuv@users.noreply.github.com>
2024-04-04 15:48:04 -0400

    Update Klutzomaniacs.java

--- homework3/Klutzomaniacs.java
+++ homework3/Klutzomaniacs.java
@@ -3,23 +3,25 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * This class provides a method to generate the binary reflected gray code for a given order and outputs the generated code as a list of bit strings.
+ * This class generates the binary reflected gray code for a given order and outputs the generated code as a list of bit strings.
  */
 public class Klutzomaniacs {
 
   public static void main(String[] args) {
+    String[] names = { "Axel", "Boxo", "Crunchy", "Doofus", "Enzo" };
+
     // order of the gray code
     int n = 5;
 
     // generate binary reflected gray code
     List<String> grayCode = brgc(n);
-    System.out.println("Binary Reflected Gray Code of Order 5: ");
 
-    int index = 0;
-    for (String code : grayCode) {
-      // print each bit string of gray code
-      System.out.println(index + ": " + code);
-      index++;
+    // print table header
+    System.out.println("Index\tGray Code\tKlutzomaniacs Riding\tAction");
+    for (int i = 0; i < grayCode.size(); i++) {
+      String subset = generateSubset(grayCode.get(i), names);
+      String action = generateAction(grayCode.get(i));
+      printRow(i, grayCode.get(i), subset, action);
     }
   }
 
@@ -56,5 +58,76 @@ public class Klutzomaniacs {
     }
 
     return grayCode;
+  }
+
+  /**
+   * This method generates the subset of Klutzomaniacs riding based on the binary code.
+   * @param code the binary code representing the Klutzomaniacs riding
+   * @param names the array of performers' names
+   * @return the subset of Klutzomaniacs riding
+   */
+  public static String generateSubset(String code, String[] names) {
+    // initialize a string to store the subset
+    String subset = "";
+
+    // add the performers' names
+    if (code.charAt(0) == '1') {
+      subset += names[0] + " & ";
+    }
+
+    for (int i = 1; i < code.length(); i++) {
+      // if the current bit is '1', add the corresponding Klutzomaniac name to the subset
+      if (code.charAt(i) == '1') {
+        subset += names[i] + " & ";
+      }
+    }
+
+    // if the subset isn't empty,
+    if (!subset.isEmpty()) {
+      // remove "&" and return
+      subset = subset.substring(0, subset.length() - 3);
+    } else {
+      // set it to "empty tricycle"
+      subset = "Empty Tricycle";
+    }
+
+    return subset;
+  }
+
+  /**
+   * This method generates the action based on the binary code.
+   * @param code the binary code representing the action
+   * @return the action that is being performed
+   */
+  public static String generateAction(String code) {
+    // if the first bit of the code is 0,
+    if (code.charAt(0) == '0') {
+      // make the action spotlight
+      return "Spotlight";
+    } else {
+      return "Joins";
+    }
+  }
+
+  /**
+   * This method builds and prints the string of the row for the table.
+   * @param index the index of the row
+   * @param grayCode the binary code
+   * @param subset the subset of Klutzomaniacs riding
+   * @param action the action that is being performed
+   */
+  public static void printRow(
+    int index,
+    String grayCode,
+    String subset,
+    String action
+  ) {
+    System.out.printf(
+      "%-6d\t%-10s\t%-30s\t%-20s%n",
+      index,
+      grayCode,
+      subset,
+      action
+    );
   }
 }
